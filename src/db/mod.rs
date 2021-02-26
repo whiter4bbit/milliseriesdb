06cb5d8fdac3b8d12b93b8d910511bc346642a8c
Pavel Zalunin <pavel.zalunin@zalando.de>
2021-02-26 20:41:36 +0100

    use async handlers

--- src/db/mod.rs
+++ src/db/mod.rs
@@ -12,16 +12,15 @@ mod test_utils;
 pub use entry::Entry;
 pub use series::{Series, SyncMode};
 use std::collections::HashMap;
+use std::fs::create_dir_all;
 use std::io;
 use std::path::{Path, PathBuf};
-use std::fs::create_dir_all;
 use std::sync::{Arc, Mutex};
 
-pub type SeriesGuard = Arc<Mutex<Series>>;
-
+#[derive(Clone)]
 pub struct DB {
     series_dir: PathBuf,
-    series: HashMap<String, SeriesGuard>,
+    series: Arc<Mutex<HashMap<String, Arc<Series>>>>,
     sync_mode: SyncMode,
 }
 
@@ -31,40 +30,40 @@ impl DB {
         create_dir_all(series_dir.clone())?;
         Ok(DB {
             series_dir: series_dir.clone(),
-            series: HashMap::new(),
+            series: Arc::new(Mutex::new(HashMap::new())),
             sync_mode: sync_mode,
         })
     }
 
-    fn get_series_internal<N: AsRef<str>>(&mut self, name: N, create: bool) -> io::Result<Option<SeriesGuard>> {
-        if self.series.contains_key(name.as_ref()) {
-            return Ok(Some(self.series.get(name.as_ref()).unwrap().clone()))
+    fn get_series_internal<N: AsRef<str>>(&mut self, name: N, create: bool) -> io::Result<Option<Arc<Series>>> {
+        let mut series = self.series.lock().unwrap();
+        if series.contains_key(name.as_ref()) {
+            return Ok(Some(series.get(name.as_ref()).unwrap().clone()));
         }
         let series_dir = self.series_dir.clone().join(name.as_ref());
         match series_dir.is_dir() || create {
             true => {
-                let series = Arc::new(Mutex::new(Series::open_or_create(series_dir, self.sync_mode)?));
-                self.series.insert(name.as_ref().to_string(), series.clone());
-                Ok(Some(series.clone()))
-            },
-            _ => Ok(None)
+                let new = Arc::new(Series::open_or_create(series_dir, self.sync_mode)?);
+                series.insert(name.as_ref().to_string(), new.clone());
+                Ok(Some(new.clone()))
+            }
+            _ => Ok(None),
         }
     }
 
-    pub fn get_series<N: AsRef<str>>(&mut self, name: N) -> io::Result<Option<SeriesGuard>> {
+    pub fn get_series<N: AsRef<str>>(&mut self, name: N) -> io::Result<Option<Arc<Series>>> {
         self.get_series_internal(name, false)
     }
 
-    pub fn create_series<N: AsRef<str>>(&mut self, name: N) -> io::Result<SeriesGuard> {
+    pub fn create_series<N: AsRef<str>>(&mut self, name: N) -> io::Result<Arc<Series>> {
         self.get_series_internal(name, true).map(|result| result.unwrap())
     }
 }
 
 #[cfg(test)]
 mod test {
-    use super::*;
     use super::test_utils::create_temp_dir;
-    
+    use super::*;
     #[test]
     fn test_db_basic() {
         let db_dir = create_temp_dir("test-base").unwrap();
@@ -72,7 +71,6 @@ mod test {
         let mut db = DB::open(&db_dir.path, SyncMode::Never).unwrap();
 
         assert!(db.get_series("co2").unwrap().is_none());
-        
         let _ = db.create_series("co2").unwrap();
 
         assert!(db.get_series("co2").unwrap().is_some());
