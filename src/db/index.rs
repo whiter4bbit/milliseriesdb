5d484b3d040c601b362cce75e4b09965aaa2a40b
whiter4bbit <wr4bbit@gmail.com>
2021-04-27 13:26:28 +0200

    refactor index

--- src/db/index.rs
+++ src/db/index.rs
@@ -35,6 +35,7 @@ impl IndexWriter {
 pub struct IndexReader {
     file: File,
     entries: u64,
+    buf: [u8; 8],
 }
 
 impl IndexReader {
@@ -42,47 +43,47 @@ impl IndexReader {
         Ok(IndexReader {
             file: io_utils::open_readable(path.as_ref().join("series.idx"))?,
             entries: offset / INDEX_ENTRY_LENGTH,
+            buf: [0u8; 8],
         })
+    }
+
+    fn read_higher_ts(&mut self, entry_index: u64) -> io::Result<u64> {
+        self.file.seek(SeekFrom::Start(entry_index * INDEX_ENTRY_LENGTH))?;
+        self.file.read_exact(&mut self.buf)?;
+
+        Ok(u64::from_be_bytes(self.buf))
+    }
+
+    fn read_offset(&mut self, entry_index: u64) -> io::Result<Option<u64>> {
+        if entry_index >= self.entries {
+            return Ok(None)
+        }
+
+        self.file.seek(SeekFrom::Start(entry_index * INDEX_ENTRY_LENGTH + 8))?;
+        self.file.read_exact(&mut self.buf)?;
+
+        Ok(Some(u64::from_be_bytes(self.buf)))
     }
 
     pub fn ceiling_offset(&mut self, target_ts: u64) -> io::Result<Option<u64>> {
         let start_ts = SystemTime::now();
         let mut scanned = 0usize;
 
-        let mut buf = [8u8; 8];
-
         let mut lo = 0i128;
         let mut hi = (self.entries - 1) as i128;
         while lo <= hi {
             let m = lo + (hi - lo) / 2;
 
-            let ts = {
-                scanned += 1;
-                
-                self.file.seek(SeekFrom::Start(m as u64 * INDEX_ENTRY_LENGTH))?;
-                self.file.read_exact(&mut buf)?;
-
-                u64::from_be_bytes(buf)
-            };
-
-            if ts < target_ts {
+            if self.read_higher_ts(m as u64)? < target_ts {
                 lo = m + 1;
             } else {
                 hi = m - 1;
             }
+
+            scanned += 1;
         }
 
-        let result = Ok(match (lo as u64) < self.entries {
-            true => {
-                scanned += 1;
-
-                self.file.seek(SeekFrom::Start(lo as u64 * INDEX_ENTRY_LENGTH + 8))?;
-                self.file.read_exact(&mut buf)?;
-
-                Some(u64::from_be_bytes(buf))
-            }
-            _ => None,
-        });
+        let result = self.read_offset(lo as u64);
 
         log::debug!("Index scanned {} entries took {}us", scanned, start_ts.elapsed().unwrap().as_micros());
         
